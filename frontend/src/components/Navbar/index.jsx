6cecdcc1cfd65be201f6af26931f6bbf34d0ae9f
Varshil-Shah <varshilashah09@gmail.com>
2023-02-18 00:12:48 +0530

    index link import

--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useAuth } from "../../context/authContext";
-
+import { Link } from "react-router-dom";
 import {
   AuthButtonGroup,
   LoginButton,
