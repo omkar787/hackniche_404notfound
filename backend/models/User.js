ae8a659feb5233a1c6afa9b527afd08d09f89947
Varshil-Shah <varshilashah09@gmail.com>
2023-02-17 12:50:39 +0530

    Fix authention bug

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,18 +1,44 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter a valid name"],
+    trim: true,
+    maxlength: [30, "Name must have less or equal then 30 characters"],
+    validate: {
+      validator: (value) => validator.isAlpha(value, "en-US", { ignore: " " }),
+      message: "A name should only contain alphabets",
+    },
+  },
   email: {
-    type: "String",
+    type: String,
+    required: [true, "Please tell us your email address"],
+    trim: true,
+    unique: true,
+    lowercase: true,
+    validate: {
+      validator: (value) => validator.isEmail(value),
+      message: "Please enter a valid email address",
+    },
   },
   password: {
-    type: "String",
-  },
-  name: {
-    type: "String",
+    type: String,
+    trim: true,
+    minlength: [8, "Please provide a password with minimum length 8."],
+    required: [true, "Please provide your password"],
+    select: false,
+    validate: {
+      validator: (value) => validator.isStrongPassword(value),
+      message:
+        "A password must contain 8 character, 1 uppercase, 1 lowercase and 1 special character",
+    },
   },
   category: {
     type: ["String"],
+    lowercase: true,
   },
 });
 
