14a080c39a79ad38bfe2a486c9e99797cb836cc7
Omkar Bhoir <71968255+omkar787@users.noreply.github.com>
2023-02-17 10:59:45 +0530

    Frontend setup

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,13 @@
+import "../css/App.css";
+import { Button } from "react-bootstrap";
+
+function App() {
+	return (
+		<div>
+			Here
+			<Button variant="primary">Here</Button>
+		</div>
+	);
+}
+
+export default App;
