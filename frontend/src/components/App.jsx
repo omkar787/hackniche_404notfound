2bdf844916f09978d6d02507f16bdcf2fdbd2076
Omkar Bhoir <71968255+omkar787@users.noreply.github.com>
2023-02-18 00:10:28 +0530

    Genre modal

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -3,19 +3,26 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
-import Genre from "../pages/Genre";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/genre" element={<Genre />} />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route
+					path="/"
+					element={<Home />}
+				/>
+				<Route
+					path="/login"
+					element={<Login />}
+				/>
+				<Route
+					path="/signup"
+					element={<Signup />}
+				/>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import SignUpForm from "../components/Auth/SignUpForm";
+import GenreModal from "../components/Auth/GenreModal";
 
 const Signup = () => {
-  return (
-    <div>
-      <Navbar />
-      <SignUpForm />
-    </div>
-  );
+	return (
+		<div>
+			<Navbar />
+			<SignUpForm />
+			<GenreModal open={true} />
+		</div>
+	);
 };
 
 export default Signup;
