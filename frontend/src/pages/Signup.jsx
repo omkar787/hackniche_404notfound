dcbaff0b8a0733c0d771468deac2fe1af878d904
Tejas <jtjtejas05@gmail.com>
2023-02-17 21:36:27 +0530

    merging in main

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,64 +1,7 @@
-import "../css/Login.css";
-import { Link } from "react-router-dom"
+import React from "react";
 
-const Sigup = () => {
-  return (
-    <div className="form-body">
-      <div className="row">
-        <div className="form-holder">
-          <div className="form-content">
-            <div className="form-items">
-              <h3>Register Here</h3>
-              <p>Fill in the data below.</p>
-              <form className="requires-validation" novalidate>
-
-              <div className="col-md-12">
-                  <input className="form-control input-field" type="text" name="text" placeholder="Full Name" required />
-                  <div className="valid-feedback">Name field is valid!</div>
-                  <div className="invalid-feedback">Name field cannot be blank!</div>
-                </div>
-
-                <div className="col-md-12">
-                  <input className="form-control input-field" type="email" name="email" placeholder="E-mail Address" required />
-                  <div className="valid-feedback">Email field is valid!</div>
-                  <div className="invalid-feedback">Email field cannot be blank!</div>
-                </div>
-
-                <div className="col-md-12">
-                  <input className="form-control input-field" type="password" name="password" placeholder="Password" required />
-                  <div className="valid-feedback">Password field is valid!</div>
-                  <div className="invalid-feedback">Password field cannot be blank!</div>
-                </div>
-                <div className="col-md-12">
-                  <input className="form-control input-field" type="password" name="confirmPassword" placeholder="Confirm Password" required />
-                  <div className="valid-feedback">Password field is valid!</div>
-                  <div className="invalid-feedback">Password field cannot be blank!</div>
-                </div>
-
-
-                <div className="col-md-12 mt-3">
-                    <span className="form-button mt-3">
-                      <button id="submit" type="submit" className="btn" style={{
-                        color: "#495056",
-                        fontWeight: "500",
-                        borderColor: "#495056",
-                      }}>Signup</button>
-                    </span>
-                    <div>
-                        Already have an account? 
-                        <Link to="/login"> Login </Link>
-                    </div>
-                    
-                </div>
-            </form>
-          </div>
-          <form className="form-item">
-          </form>
-        </div>
-      </div>
-    </div>
-</div >
-  );
+const Signup = () => {
+  return <div>Signup</div>;
 };
 
-export default Sigup;
+export default Signup;
