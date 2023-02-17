58281cb991cf8fa3a8c460ddec04ac223ceb061c
Varshil-Shah <varshilashah09@gmail.com>
2023-02-17 15:46:21 +0530

    Add timestamps in all models

--- backend/models/Country.js
+++ backend/models/Country.js
@@ -1,12 +1,23 @@
 const mongoose = require("mongoose");
 
-const CountrySchema = new mongoose.Schema({
-  name: {
-    type: "String",
+const CountrySchema = new mongoose.Schema(
+  {
+    name: {
+      type: "String",
+    },
+    code: {
+      type: "String",
+    },
   },
-  code: {
-    type: "String",
-  },
-});
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("country", CountrySchema);
--- backend/models/News.js
+++ backend/models/News.js
@@ -1,56 +1,67 @@
 const mongoose = require("mongoose");
 
-const NewsSchema = new mongoose.Schema({
-  title: {
-    type: "String",
+const NewsSchema = new mongoose.Schema(
+  {
+    title: {
+      type: "String",
+    },
+    link: {
+      type: "String",
+    },
+    keywords: {
+      type: ["String"],
+    },
+    creator: {
+      type: ["String"],
+    },
+    video_url: {
+      type: "String",
+    },
+    description: {
+      type: "String",
+    },
+    content: {
+      type: "String",
+    },
+    pubDate: {
+      type: "Date",
+    },
+    full_description: {
+      type: "String",
+    },
+    image_url: {
+      type: "String",
+    },
+    source_id: {
+      type: "String",
+    },
+    country: {
+      type: ["String"],
+    },
+    category: {
+      type: ["String"],
+    },
+    language: {
+      type: "String",
+    },
+    like_count: {
+      type: Number,
+      default: 0,
+    },
+    comment_count: {
+      type: Number,
+      default: 0,
+    },
   },
-  link: {
-    type: "String",
-  },
-  keywords: {
-    type: ["String"],
-  },
-  creator: {
-    type: ["String"],
-  },
-  video_url: {
-    type: "String",
-  },
-  description: {
-    type: "String",
-  },
-  content: {
-    type: "String",
-  },
-  pubDate: {
-    type: "Date",
-  },
-  full_description: {
-    type: "String",
-  },
-  image_url: {
-    type: "String",
-  },
-  source_id: {
-    type: "String",
-  },
-  country: {
-    type: ["String"],
-  },
-  category: {
-    type: ["String"],
-  },
-  language: {
-    type: "String",
-  },
-  like_count: {
-    type: Number,
-    default: 0,
-  },
-  comment_count: {
-    type: Number,
-    default: 0,
-  },
-});
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("news", NewsSchema);
