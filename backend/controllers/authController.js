f746c569d0e6da01869752ac3007e75fcaa62e8e
Omkar Bhoir <71968255+omkar787@users.noreply.github.com>
2023-02-18 10:51:52 +0530

    Problem in hosted site

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -6,150 +6,151 @@ const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
 
 const signToken = (id) =>
-  jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+	jwt.sign({ id }, process.env.JWT_SECRET, {
+		expiresIn: process.env.JWT_EXPIRES_IN,
+	});
 
 const createSendToken = (user, statusCode, res) => {
-  const cookieOptions = {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true,
-    secure: true,
-  };
+	const cookieOptions = {
+		expires: new Date(
+			Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+		),
+		httpOnly: true,
+		secure: true,
+		sameSite: "None",
+	};
 
-  const token = signToken(user._id);
+	const token = signToken(user._id);
 
-  res.cookie("jwt", token, cookieOptions);
+	res.cookie("jwt", token, cookieOptions);
 
-  // Remove password from the output
-  user.password = undefined;
+	// Remove password from the output
+	user.password = undefined;
 
-  res.status(statusCode).json({
-    status: "success",
-    token,
-    data: {
-      user,
-    },
-  });
+	res.status(statusCode).json({
+		status: "success",
+		token,
+		data: {
+			user,
+		},
+	});
 };
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const user = await User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-    category: req.body.category,
-  });
+	const user = await User.create({
+		name: req.body.name,
+		email: req.body.email,
+		password: req.body.password,
+		category: req.body.category,
+	});
 
-  createSendToken(user, 201, res);
+	createSendToken(user, 201, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
-  const { email, password } = req.body;
+	const { email, password } = req.body;
 
-  // Check if the email and password exists
-  if (!email || !password) {
-    return next(new AppError("Please provide email and password.", 400));
-  }
+	// Check if the email and password exists
+	if (!email || !password) {
+		return next(new AppError("Please provide email and password.", 400));
+	}
 
-  // Check if the user exists and password is correct
-  const user = await User.findOne({ email }).select("password");
+	// Check if the user exists and password is correct
+	const user = await User.findOne({ email }).select("password");
 
-  if (!user || !(await user.verifyPassword(password, user.password))) {
-    return next(
-      new AppError(`Email doesn't exists or password is incorrect`, 401)
-    );
-  }
+	if (!user || !(await user.verifyPassword(password, user.password))) {
+		return next(
+			new AppError(`Email doesn't exists or password is incorrect`, 401)
+		);
+	}
 
-  createSendToken(user, 200, res);
+	createSendToken(user, 200, res);
 });
 
 exports.logout = (req, res) => {
-  res.cookie("jwt", "logout", {
-    expires: new Date(Date.now() + 10 * 1000),
-    httpOnly: true,
-  });
-  res.status(200).json({
-    status: "success",
-  });
+	res.cookie("jwt", "logout", {
+		expires: new Date(Date.now() + 10 * 1000),
+		httpOnly: true,
+	});
+	res.status(200).json({
+		status: "success",
+	});
 };
 
 exports.protect = catchAsync(async (req, res, next) => {
-  // check if the token is present
-  let token;
+	// check if the token is present
+	let token;
 
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
-    token = req.headers.authorization.split(" ")[1];
-  } else if (req.cookies.jwt) {
-    token = req.cookies.jwt;
-  }
+	if (
+		req.headers.authorization &&
+		req.headers.authorization.startsWith("Bearer")
+	) {
+		token = req.headers.authorization.split(" ")[1];
+	} else if (req.cookies.jwt) {
+		token = req.cookies.jwt;
+	}
 
-  if (!token) {
-    return next(
-      new AppError("You are not logged in! Please login to get access", 401)
-    );
-  }
+	if (!token) {
+		return next(
+			new AppError("You are not logged in! Please login to get access", 401)
+		);
+	}
 
-  // verification of the token
-  const decode = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+	// verification of the token
+	const decode = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
-  // check if the user still exists
-  const currentUser = await User.findById(decode.id);
-  if (!currentUser) {
-    return next(
-      new AppError(`The user belonging to this token no longer exists`, 401)
-    );
-  }
+	// check if the user still exists
+	const currentUser = await User.findById(decode.id);
+	if (!currentUser) {
+		return next(
+			new AppError(`The user belonging to this token no longer exists`, 401)
+		);
+	}
 
-  req.user = currentUser;
-  next();
+	req.user = currentUser;
+	next();
 });
 
 exports.verifyLoggedIn = async (req, res, next) => {
-  // check if the token is present
-  if (req.cookies.jwt) {
-    try {
-      // verification of the token
-      const decode = await promisify(jwt.verify)(
-        req.cookies.jwt,
-        process.env.JWT_SECRET
-      );
+	// check if the token is present
+	if (req.cookies.jwt) {
+		try {
+			// verification of the token
+			const decode = await promisify(jwt.verify)(
+				req.cookies.jwt,
+				process.env.JWT_SECRET
+			);
 
-      // check if the user still exists
-      const currentUser = await User.findById(decode.id);
-      if (!currentUser) {
-        return res.status(200).json({
-          ok: false,
-        });
-      }
+			// check if the user still exists
+			const currentUser = await User.findById(decode.id);
+			if (!currentUser) {
+				return res.status(200).json({
+					ok: false,
+				});
+			}
 
-      return res.json({
-        ok: true,
-        user: currentUser,
-      });
-    } catch (error) {
-      return res.status(200).json({
-        ok: false,
-      });
-    }
-  }
-  return res.status(200).json({
-    ok: false,
-  });
+			return res.json({
+				ok: true,
+				user: currentUser,
+			});
+		} catch (error) {
+			return res.status(200).json({
+				ok: false,
+			});
+		}
+	}
+	return res.status(200).json({
+		ok: false,
+	});
 };
 
 exports.restrictTo =
-  (...roles) =>
-  (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
-      return next(
-        new AppError(`You do not have permission to perform this action!`, 403)
-      );
-    }
-    next();
-  };
+	(...roles) =>
+	(req, res, next) => {
+		if (!roles.includes(req.user.role)) {
+			return next(
+				new AppError(`You do not have permission to perform this action!`, 403)
+			);
+		}
+		next();
+	};
