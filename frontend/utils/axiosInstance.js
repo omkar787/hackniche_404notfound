e7bac6eefb7005391ae84478588c6600887bb76f
Tejas <jtjtejas05@gmail.com>
2023-02-18 10:23:15 +0530

    url

--- frontend/utils/axiosInstance.js
+++ frontend/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "https://atinlxlnjh.execute-api.ap-south-1.amazonaws.com/dev/api/v1",
+  baseURL: "https://fragile-panama-hat-newt.cyclic.app/api/v1",
   withCredentials: "include",
 });
 
